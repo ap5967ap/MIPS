d15c41f3a58346cee09888c186c34d7ba21e1d34
ap5967ap <aditya.priyadarshi@iiitb.ac.in>
2023-10-01 23:25:31 +0530

    Added feature - Data Memory Segment

--- assembler2.py
+++ assembler2.py
@@ -6,12 +6,13 @@ import sys
 from source2basic import *
 inFile = sys.argv[1]
 outFile = sys.argv[2]
-
+memFile = sys.argv[3]
 with open(inFile,'r') as i:
     lines = i.readlines()
 
 code=pd.DataFrame(columns=['address','machine_code','basic code'])
-
+data_memory_mips=pd.DataFrame(columns=['address','data'])
+data_memory=[]
 text=0 # flag to see if we are in the .text section of the file 
 
 # tells if a line contains a label definition 
@@ -22,9 +23,12 @@ def labelDef(line):
 
 pc = 0x00400000
 # first pass
+data = 0x10010000
 def first_pass(lines:list[str]):
     global code
     global pc
+    global data
+    global data_memory
     line = 0
     temp=0
     for line_str in lines:
@@ -47,12 +51,23 @@ def first_pass(lines:list[str]):
                 if string.startswith(".asciiz"):
                     string = string[7:].strip()
                     data_elements[label]= [str(temp),(len(string)-1)]
+                    cc=0
+                    for i in string :
+                        # data_memory.append(bin_string(ord(i),8))
+                        if i == '\\' and string[cc+1] == 'n':
+                            cc+=1
+                            data_memory.append(r'\n')
+                        data_memory.append(i)
+                        cc+=1
+                    data_memory.append('\0')
                     if r"\n" in string:
                         data_elements[label][1] -= 1
                     temp+=data_elements[label][1]
                 elif string.startswith(".space"):
                     string=string[6:].strip()
                     data_elements[label]=[str(temp),int(string)]
+                    for i in range(int(string)):
+                        data_memory.append('\0')
                     temp+=int(string)
         if betterLine.startswith('.text'):
             text = 1
@@ -164,14 +179,68 @@ def secondPass():
             number = number[:-2]
             temp=bin_string(Jtype[bc_parts[0]], 6) + number
             i['machine_code'] = bin_to_hex_str(temp)
-            # i['machine_code'] = bin_to_hex_str(temp)
                 
 
 
 
 first_pass(lines)
 secondPass()
-# print(symbol_table['loopend2'])
-# print()
-
+for idx, i in code.iterrows():
+    i['address'] = str(hex(int(i['address'])))
 csv=code.to_csv(outFile, index=False,sep='\t')
+data_mem=[]
+for i in range(len(data_memory)):
+    if data_memory[i] == '"' or data_memory[i] == '\\' or ( data_memory[i] == 'n' and data_memory[i-1] == '\\n'):
+        continue
+    data_mem.append(data_memory[i])
+    
+data_memory = []
+for i in range(len(data_mem)):
+    if i and data_mem[i] == 'n' and data_mem[i-1] == '\\n':
+        continue
+    data_memory.append(data_mem[i])
+
+if len(data_memory)%32:
+    left=32-len(data_memory)%32
+
+for i in range(left):
+    data_memory.append('\0')
+    
+    
+temp_list=[]
+
+for i in range(0,len(data_memory),4):
+    x=data_memory[i]
+    y=data_memory[i+1]
+    z=data_memory[i+2]
+    w=data_memory[i+3]
+    if x =='\\n':
+        x='\n'
+    if y =='\\n':
+        y='\n'
+    if z =='\\n':
+        z='\n'
+    if w =='\\n':
+        w='\n'
+    x=bin_string(ord(x),8)
+    y=bin_string(ord(y),8)
+    z=bin_string(ord(z),8)
+    w=bin_string(ord(w),8)
+    res=w+z+y+x #?writing in liitle endian format
+    temp_list.append(bin_to_hex_str(res))
+    
+for i in range(0,len(temp_list),8):
+    
+    a=temp_list[i]
+    b=temp_list[i+1]
+    c=temp_list[i+2]
+    d=temp_list[i+3]
+    e=temp_list[i+4]
+    f=temp_list[i+5]
+    g=temp_list[i+6]
+    h=temp_list[i+7]
+    temp22=a+" "+b+" "+c+" "+d+" "+e+" "+f+" "+g+" "+h
+    data_memory_mips=data_memory_mips._append({'address':str(hex(data)),'data':temp22},ignore_index=True)
+    data+=32
+    
+data_file=data_memory_mips.to_csv(memFile, index=False,sep='\t')
